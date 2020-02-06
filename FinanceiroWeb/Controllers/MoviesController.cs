298f1e0807add79ab8ed1d5ef9b61fbac00faad0
Joao Victor de A. Monteiro <joao.monteiro@fitbank.com.br>
2020-02-06 15:29:31 -0300

    Projeto MVC Completo em ingles

---
+++ FinanceiroWeb/Controllers/MoviesController.cs
@@ -0,0 +1,22 @@
+﻿using FinanceiroWeb.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FinanceiroWeb.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies
+        public ActionResult Random()
+        {
+            Movie movie = new Movie()
+            {
+                Name = "Fred"
+        };
+            return View(movie);
+        }
+    }
+}
